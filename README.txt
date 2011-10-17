c51367fed44023f0e47d0d67dbf307d69ab46382
Andrea Romanello <rommelandrea@gmail.com>
2011-10-17 16:08:38 +0200

    aggiunto readme

---
+++ README.txt
@@ -0,0 +1,1 @@
+segmento somma massima
---
+++ ssm.c
@@ -0,0 +1,18 @@
+/* 
+ * File:   ssm.c
+ * Author: rommel
+ *
+ * Created on October 17, 2011, 4:05 PM
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return (EXIT_SUCCESS);
+}
+
