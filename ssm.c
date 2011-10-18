88a9abab985f03adff92a0e644c8db1a2ccd9f40
Andrea Romanello <rommelandrea@gmail.com>
2011-10-18 12:51:37 +0200

    sistemata random sistemato maxPeriodoGelo inseriti commenti

--- ssm.c
+++ ssm.c
@@ -6,6 +6,7 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 
 #define MIN -10
 #define MAX 10
@@ -13,7 +14,7 @@
 /*
  * 
  */
-struct SegmentoStruct{
+struct SegmentoStruct {
     int somma;
     int inizio;
     int fine;
@@ -28,57 +29,85 @@ Segmento ssm(int [], int);
 void riempiArrayCasuale(int [], int, signed int, signed int);
 void print(int [], int);
 
-void riempiArrayCasuale(int a[], int length, signed int max, signed int min){
+/**
+ * riempie un array di numeri casuali compresi tra min e max
+ * @param a array
+ * @param length lunghezza array
+ * @param min valore minim
+ * @param max valore massimo
+ */
+void riempiArrayCasuale(int a[], int length, int min, int max) {
     int i;
-    
+
     srand(time(NULL));
-    
-    for(i=0; i<length; i++)
-        a[i] = (rand()%(max+min))-min;
+
+    for (i = 0; i < length; i++)
+        a[i] = ((rand() % (max - min)) + min);
 }
 
-void print(int a[], int length){
+/**
+ * questa funzione stampa l'array
+ * @param a array
+ * @param length lunghezza array
+ */
+void print(int a[], int length) {
     int i;
     printf("stampo l'array\n");
-    for(i=0; i<length; i++){
+    for (i = 0; i < length; i++) {
         printf("%d-->", a[i]);
     }
     printf("\n");
 }
 
-int maxPeriodoGelo(int a[], int length){
-    int max, temp, i, j = 0;
-    
-    for(i=0; i<length; i++){
-        for(j=i; j<length; j++){
-            if(a[j]<0){
-                temp++;
-                if(temp>max)
-                    max=temp;
-            }else{
-                temp = 0;
-            }
+/**
+ * questa funzione restituisce il numero massimo di valori negativi consecutivi
+ * @param a array
+ * @param length lunghezza dell'array
+ * @return numero massimo di elementi negativi
+ */
+int maxPeriodoGelo(int a[], int length) {
+    int max, temp, i;
+    max = temp = 0;
+
+    for (i = 0; i < length; i++) {
+        if (a[i] < 0) {
+            temp++;
+            if (temp > max)
+                max = temp;
+        } else {
+            temp = 0;
         }
     }
-    
     return max;
 }
 
+/**
+ * main del programma
+ * @param argc
+ * @param argv
+ * @return 
+ */
 int main(int argc, char** argv) {
-    int length;
+    int length, periodo;
     length = 10;
-    
-    signed int min = -10;
-    signed int max = 10;
-    
-    signed int a[length];
-    
-    printf("%d", max+min);
-    
+
+    int min = -10;
+    int max = 10;
+
+    int a[length];
+
+    printf("stampo min + max ");
+    printf("%d\n", max + min);
+
+    printf("entro in riempi casuale\n");
     riempiArrayCasuale(a, length, min, max);
-    
-    //print(a, length);
-    
+
+    print(a, length);
+
+    periodo = maxPeriodoGelo(a, length);
+
+    printf("\n\nil max periodo di gelo e' %d", periodo);
+
     return (EXIT_SUCCESS);
 }
 
